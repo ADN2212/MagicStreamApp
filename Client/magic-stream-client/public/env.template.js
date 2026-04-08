ca54e94cda2f2d2d658d9f1ef72123cc1c2e5e89
Juan Adonis <adn221294@gmail.com>
2026-04-08 12:00:52 -0400

    Additional configurations to add environment variables to the client from the root .env file via Docker Compose

---
+++ Client/magic-stream-client/public/env.template.js
@@ -0,0 +1,4 @@
+//Esto sirve para que al correr docker-compose se puedan inyeactar las envs que estan en el root del proyecto. 
+window.__ENV__ = {
+    API_URL: "$API_URL"
+}
--- Client/magic-stream-client/src/api/axiosConfig.js
+++ Client/magic-stream-client/src/api/axiosConfig.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-const apiUrl = import.meta.env.VITE_API_BASE_URL;
+//const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+//Esto es para tomar la API_URL que se inyeacta desde el docker-compose.
+const apiUrl = window.__ENV__?.API_URL;
 
 export default axios.create({
     baseURL:apiUrl,
